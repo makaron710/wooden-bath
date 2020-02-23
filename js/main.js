6b5c59fe37ed4a2e59e5e2e88cec0676bea7e17c
Максим Шаров <sm710mh@gmail.com>
2020-02-23 21:56:48 +0300

    feat: adaptiv hero section & desktop about section

--- js/main.js
+++ js/main.js
@@ -1,11 +1,30 @@
 $(document).ready(function () {
+
   //initialize swiper when document ready
   var mySwiper = new Swiper('.swiper-container', {
     // Optional parameters
+
+    speed: 1000,
     loop: true,
     navigation: {
       nextEl: '.swiper__button-next',
       prevEl: '.swiper__button-prev'
-    }
+    },
+    autoplay: {
+      delay: 4000
+    },
+    effect: 'fade'
   });
+
+  if (window.innerWidth >= 768) {
+    mySwiper.autoplay.stop();
+  }
+
+  window.addEventListener('resize', function() {
+    if (window.innerWidth >= 768) {
+      mySwiper.autoplay.stop();
+    } else {
+      mySwiper.autoplay.start();
+    }
+  })
 }); 
