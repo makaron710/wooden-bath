777784d703fd03a91aa8a844eb689c1bdd31ab91
Максим Шаров <sm710mh@gmail.com>
2020-02-27 16:42:17 +0300

    feat: add customers section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-  //initialize swiper-s1
+  //initialize swiper
   var mySwiper = new Swiper('.swiper-container', {
     // Optional parameters
 
@@ -16,43 +16,97 @@ $(document).ready(function () {
     effect: 'fade'
   });
 
-  if (window.innerWidth >= 768) {
+  if (document.body.clientWidth >= 768) {
     mySwiper.autoplay.stop();
   }
 
   window.addEventListener('resize', function() {
-    if (window.innerWidth >= 768) {
+    if (document.body.clientWidth >= 768) {
       mySwiper.autoplay.stop();
     } else {
       mySwiper.autoplay.start();
     }
   })
 
-  var mySwiper2 = new Swiper('.swiper-container-s2', {
-    // Optional parameters
+  //initialize swiper-s2
 
+  let initializedS2 = false;
+  var paginationS2 = document.querySelector('.swiper-pagination');
+  
+  if (document.body.clientWidth <= 767) {
+    paginationS2.style.display = 'block';
+    var mySwiper2 = new Swiper('.swiper-container-s2', {
+      // Optional parameters
+      speed: 1000,
+      loop: true,
+      navigation: {
+        nextEl: '.swiper__button-next-s2',
+        prevEl: '.swiper__button-prev-s2'
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets'
+      },
+      autoplay: {
+        delay: 4000
+      },
+      effect: 'fade'
+    });
+
+    initializedS2 = true;
+  }
+
+  window.addEventListener('resize', function() {
+    if (document.body.clientWidth >= 767) {
+      if (initializedS2 === true) {
+        mySwiper2.destroy();
+        initializedS2 = false;
+        paginationS2.style.display = 'none';
+      }
+    } else {
+      if (initializedS2 === false) {
+        paginationS2.style.display = 'block';
+        mySwiper2 = new Swiper('.swiper-container-s2', {
+          // Optional parameters
+          speed: 1000,
+          loop: true,
+          navigation: {
+            nextEl: '.swiper__button-next-s2',
+            prevEl: '.swiper__button-prev-s2'
+          },
+          pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets'
+          },
+          autoplay: {
+            delay: 4000
+          },
+          effect: 'fade'
+        });
+    
+        initializedS2 = true;
+        /* document.location.href = '#projects'; */
+      }
+    }
+  })
+
+  //initialize swiper-s3
+  var mySwiper3  = new Swiper('.swiper-container-s3', {
+    // Optional parameters
+    initialSlide: 1,
     speed: 1000,
     loop: true,
     navigation: {
-      nextEl: '.swiper__button-next-s2',
-      prevEl: '.swiper__button-prev-s2'
+      nextEl: '.swiper__button-next-s3',
+      prevEl: '.swiper__button-prev-s3'
     },
     pagination: {
-      el: '.swiper-pagination',
+      el: '.swiper-pagination-s3',
       type: 'bullets'
     },
-    autoplay: {
-      delay: 4000
-    },
-    effect: 'fade'
-  });
 
-  //initialize swiper-s2
-  window.addEventListener('resize', function() {
-    if (window.innerWidth >= 767) {
-      mySwiper2.destroy();
-    }
-  })
+    /* effect: 'fade' */
+  });
 
 
 }); 
